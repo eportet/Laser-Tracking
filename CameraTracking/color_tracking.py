a71d50c867a0bf87cf2eaf6696ff193715a04392
Eduardo Portet <eduardoportet@gmail.com>
2018-03-27 14:25:52 -0400

    Servo controller working

--- CameraTracking/color_tracking.py
+++ CameraTracking/color_tracking.py
@@ -3,123 +3,95 @@ import numpy as np
 import serial
 import os
 
-# open serial communication
-run_serial = False
-port_name = os.popen("ls /dev/ttyACM*").read().rstrip()
-if (port_name != ""):
-    run_serial = True
-    port = serial.Serial(port_name)
+from helper_functions import setup, getBounds, moveServo
 
-kernel = np.ones((5,5),np.uint8)
+display_windows = True # Set to true to open all windows or false for none
+run_serial = False # Do not change
 
-# Take input from webcam
-# sudo modprobe bcm2835-v4l2 makes the Raspberry Pi camera visible
-cap = cv2.VideoCapture(0)
-print("Video Capture Opened")
+# Pinkish
+lowerbound = (165,90,40)
+upperbound = (179,190,205)
 
-# Reduce the size of video to 320x240 so rpi can process faster
-cap.set(3,320)
-cap.set(4,240)
+cap, port, p_h, p_v = setup(lowerbound, upperbound, display_windows)
 
-def nothing(x):
-    pass
+if port != None:
+	port = serial.Serial(port_name)
+	run_serial = True
 
-def makeWindows(lb, ub):
-    # Creating a windows for later use
-    cv2.namedWindow('HueComp')
-    cv2.namedWindow('SatComp')
-    cv2.namedWindow('ValComp')
-    cv2.namedWindow('closing')
-    cv2.namedWindow('tracking')
-
-
-    # Creating track bar for min and max for hue, saturation and value
-    # You can adjust the defaults as you like
-    cv2.createTrackbar('hmin', 'HueComp',lb[0],179,nothing)
-    cv2.createTrackbar('hmax', 'HueComp',ub[0],179,nothing)
-
-    cv2.createTrackbar('smin', 'SatComp',lb[1],255,nothing)
-    cv2.createTrackbar('smax', 'SatComp',ub[1],255,nothing)
-
-    cv2.createTrackbar('vmin', 'ValComp',lb[2],255,nothing)
-    cv2.createTrackbar('vmax', 'ValComp',ub[2],255,nothing)
-
-def getBounds():
-    hmn = cv2.getTrackbarPos('hmin','HueComp')
-    hmx = cv2.getTrackbarPos('hmax','HueComp')
-    
-    smn = cv2.getTrackbarPos('smin','SatComp')
-    smx = cv2.getTrackbarPos('smax','SatComp')
-
-    vmn = cv2.getTrackbarPos('vmin','ValComp')
-    vmx = cv2.getTrackbarPos('vmax','ValComp')
-    
-    return (hmn,smn,vmn), (hmx,smx,vmx)
-
-lowerbound = (12,145,186)
-upperbound = (37,255,255)
-makeWindows(lowerbound, upperbound)
+dc_h = 6.0
+dc_v = 4.5
+p_h.start(dc_h)
+p_v.start(dc_v)
 
 while(1):
+	# MOVING SERVO
+	p_h.ChangeDutyCycle(dc_h)
+	p_v.ChangeDutyCycle(dc_v)
 
-    buzz = 0
-    _, frame = cap.read()
+	# Read frame
+	_, frame = cap.read()
 
-    #Blur
-    blurred = cv2.GaussianBlur(frame, (5,5), 0)
+	# Blur the frame
+	blurred = cv2.GaussianBlur(frame, (5,5), 0)
 
-    #converting to HSV
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    hue,sat,val = cv2.split(hsv)
+	# Converting to HSV
+	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+	hue,sat,val = cv2.split(hsv)
 
-    # get info from track bar and appy to result
-    lowerbound, upperbound = getBounds()
+	# Get info from track bar and appy to result
+	if display_windows:
+		lowerbound, upperbound = getBounds()
 
-    # Apply thresholding
-    hthresh = cv2.inRange(np.array(hue), np.array(lowerbound[0]), np.array(upperbound[0]))
-    sthresh = cv2.inRange(np.array(sat), np.array(lowerbound[1]), np.array(upperbound[1]))
-    vthresh = cv2.inRange(np.array(val), np.array(lowerbound[2]), np.array(upperbound[2]))
+	# construct a mask from the bounds obtained from trackbars, then perform
+	# a series of dilations and erosions to remove any small
+	# blobs left in the mask
+	mask = cv2.inRange(hsv, lowerbound, upperbound)
+	mask = cv2.erode(mask, None, iterations=2)
+	closing = cv2.dilate(mask, None, iterations=2)
 
-    # construct a mask from the bounds obtained from trackbars, then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, lowerbound, upperbound)
-    mask = cv2.erode(mask, None, iterations=2)
-    closing = cv2.dilate(mask, None, iterations=2)
-
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
+	# find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
  
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        if M["m00"] != 0:
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            port.write(str(center)+"\n") if run_serial else None
-        
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame
-            cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		if M["m00"] != 0:
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			dc_h, dc_v = moveServo(center[0],center[1], dc_h, dc_v)
+			port.write(str(center)+"\n") if run_serial else None
+		
+		# only proceed if the radius meets a minimum size
+		if radius > 10:
+			# draw the circle and centroid on the frame
+			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-    #Show the result in frames
-    cv2.imshow('HueComp',hthresh)
-    cv2.imshow('SatComp',sthresh)
-    cv2.imshow('ValComp',vthresh)
-    cv2.imshow('closing',closing)
-    cv2.imshow('tracking',frame)
 
-    k = cv2.waitKey(1) & 0xFF
-    if k == ord("q"):
-        break
+	if display_windows:
+		
+		hthresh = cv2.inRange(np.array(hue), np.array(lowerbound[0]), np.array(upperbound[0]))
+		sthresh = cv2.inRange(np.array(sat), np.array(lowerbound[1]), np.array(upperbound[1]))
+		vthresh = cv2.inRange(np.array(val), np.array(lowerbound[2]), np.array(upperbound[2]))
+
+		#Show the result in frames
+		cv2.imshow('HueComp',hthresh)
+		cv2.imshow('SatComp',sthresh)
+		cv2.imshow('ValComp',vthresh)
+		cv2.imshow('closing',closing)
+
+	#frame = np.rot90(np.rot90(frame)) # Rotate frame
+	cv2.imshow('tracking',frame)
+
+	k = cv2.waitKey(1) & 0xFF
+	if k == ord("q"):
+		break
 
 cap.release()
 cv2.destroyAllWindows()
