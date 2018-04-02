74b4897ac6942f98d0a7e3f6f6e5c5f54cf395b3
Eduardo Portet <eportet@dhcp-wifi-8021x-155-41-23-103.bu.edu>
2018-04-02 12:30:33 -0400

    format fix

--- CameraTracking/coarse.py
+++ CameraTracking/coarse.py
@@ -5,13 +5,122 @@ import os
 import RPi.GPIO as GPIO
 import time
 
-
-####################
-# GLOBAL VARIABLES #
-####################
-
+#################
+# MAIN FUNCTION #
+#################
 
 
+def main(u, s, w, lb, ub):
+
+	####################
+	# GLOBAL VARIABLES #
+	####################
+
+	usb = False		# True : Try to communicate with serial
+					# False: Do not communicate
+
+	servo = True	# True : Communicate with the servos
+					# False: Do not initiate servos
+
+	windows = 2		# 0: Do not show any windows
+					# 1: Show only the tracked window
+					# 2: Show all the windows
+
+	# Set the (h,s,v) lower and upper bounds
+	lowerbound = (0,80,80)
+	upperbound = (10,255,200)
+
+	# Duty Cycle Values
+	# set to rest values
+	dc_p = 6.0
+	dc_t = 4.5
+
+	cap, port, pwm_p, pwm_t = setup(usb, servo, windows, lowerbound, upperbound)
+
+	##############
+	# BEGIN LOOP #
+	##############
+
+	while(1):
+
+		# MOVING SERVO
+		#pwm_p.ChangeDutyCycle(dc_p)
+		pwm_t.ChangeDutyCycle(dc_t)
+
+		# Read frame
+		_, frame = cap.read()
+
+		# Blur the frame
+		blurred = cv2.GaussianBlur(frame, (5,5), 0)
+
+		# Converting to HSV
+		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+		hue,sat,val = cv2.split(hsv)
+
+		# Get info from track bar and appy to result
+		if windows == 2:
+			lowerbound, upperbound = getBounds()
+
+		# construct a mask from the bounds obtained from trackbars, then perform
+		# a series of dilations and erosions to remove any small
+		# blobs left in the mask
+		mask = cv2.inRange(hsv, lowerbound, upperbound)
+		mask = cv2.erode(mask, None, iterations=2)
+		closing = cv2.dilate(mask, None, iterations=2)
+
+		# find contours in the mask and initialize the current
+		# (x, y) center of the ball
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+		center = None
+
+		# only proceed if at least one contour was found
+		if len(cnts) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cnts, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			if M["m00"] != 0:
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+				#dc_p, dc_t = moveServo(center[0],center[1], dc_p, dc_t)
+				port.write(str(center)+"\n") if usb else None
+
+			# only proceed if the radius meets a minimum size
+			if radius > 10:
+				# draw the circle and centroid on the frame
+				cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+				cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+		# display wanted windows
+		if windows == 1 or windows == 2:
+
+			frame = np.rot90(np.rot90(frame)) # Rotate frame
+			cv2.imshow('tracking',frame)
+
+			if windows == 2:
+				hthresh = cv2.inRange(np.array(hue), np.array(lowerbound[0]), np.array(upperbound[0]))
+				sthresh = cv2.inRange(np.array(sat), np.array(lowerbound[1]), np.array(upperbound[1]))
+				vthresh = cv2.inRange(np.array(val), np.array(lowerbound[2]), np.array(upperbound[2]))
+
+				hthresh = np.rot90(np.rot90(hthresh)) # Rotate frame
+				sthresh = np.rot90(np.rot90(sthresh)) # Rotate frame
+				vthresh = np.rot90(np.rot90(vthresh)) # Rotate frame
+
+				#Show the result in frames
+				cv2.imshow('HueComp',hthresh)
+				cv2.imshow('SatComp',sthresh)
+				cv2.imshow('ValComp',vthresh)
+				cv2.imshow('closing',closing)
+
+		# exit when 'q' key is pressed
+		k = cv2.waitKey(1) & 0xFF
+		if k == ord("q"):
+			break
+
+	#GPIO.cleanup()
+	cap.release()
+	cv2.destroyAllWindows()
 
 
 ####################
@@ -23,7 +132,7 @@ def nothing(x):
 	pass
 
 def setup(u, s, w, lb, ub):
-	# CAMERA SETUP 
+	# CAMERA SETUP
 	# sudo modprobe bcm2835-v4l2 makes the Raspberry Pi camera visible
 	kernel = np.ones((5,5),np.uint8)
 	cap = cv2.VideoCapture(0)
@@ -83,13 +192,13 @@ def makeTrackbars(lb, ub):
 def getBounds():
 	hmn = cv2.getTrackbarPos('hmin','HueComp')
 	hmx = cv2.getTrackbarPos('hmax','HueComp')
-	
+
 	smn = cv2.getTrackbarPos('smin','SatComp')
 	smx = cv2.getTrackbarPos('smax','SatComp')
 
 	vmn = cv2.getTrackbarPos('vmin','ValComp')
 	vmx = cv2.getTrackbarPos('vmax','ValComp')
-	
+
 	return (hmn,smn,vmn), (hmx,smx,vmx)
 
 def moveServo(x, y, dcx, dcy):
@@ -107,120 +216,13 @@ def moveServo(x, y, dcx, dcy):
 	elif x < 80 and dcx > 2.25:	# MOVE RIGHT
 		dcx = dcx - 0.05
 		print("RIGHT x:" + str(x) + " dc: " + str(dcx))
-	
+
 
 	return dcx, dcy
 
-
-########################
-# DEFINE MAIN FUNCTION #
-########################
-
-
-def main():
-	
-	usb = False		# True : Try to communicate with serial
-				# False: Do not communicate
-
-	servo = True	# True : Communicate with the servos
-					# False: Do not initiate servos
-	
-	windows = 2		# 0: Do not show any windows
-					# 1: Show only the tracked window 
-					# 2: Show all the windows
-	
-	# Set the (h,s,v) lower and upper bounds
-	lowerbound = (0,80,80)
-	upperbound = (10,255,200)
-	
-	# Duty Cycle Values
-	# set to rest values
-	dc_p = 6.0
-	dc_t = 4.5
-
-	cap, port, pwm_p, pwm_t = setup(usb, servo, windows, lowerbound, upperbound)
-
-
-	while(1):
-		
-		# MOVING SERVO
-		#pwm_p.ChangeDutyCycle(dc_p)
-		pwm_t.ChangeDutyCycle(dc_t)
-
-		# Read frame
-		_, frame = cap.read()
-
-		# Blur the frame
-		blurred = cv2.GaussianBlur(frame, (5,5), 0)
-
-		# Converting to HSV
-		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-		hue,sat,val = cv2.split(hsv)
-
-		# Get info from track bar and appy to result
-		if windows == 2:
-			lowerbound, upperbound = getBounds()
-
-		# construct a mask from the bounds obtained from trackbars, then perform
-		# a series of dilations and erosions to remove any small
-		# blobs left in the mask
-		mask = cv2.inRange(hsv, lowerbound, upperbound)
-		mask = cv2.erode(mask, None, iterations=2)
-		closing = cv2.dilate(mask, None, iterations=2)
-
-		# find contours in the mask and initialize the current
-		# (x, y) center of the ball
-		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-		center = None
-	 
-		# only proceed if at least one contour was found
-		if len(cnts) > 0:
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			if M["m00"] != 0:
-				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-				#dc_p, dc_t = moveServo(center[0],center[1], dc_p, dc_t)
-				port.write(str(center)+"\n") if usb else None
-			
-			# only proceed if the radius meets a minimum size
-			if radius > 10:
-				# draw the circle and centroid on the frame
-				cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
-				cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-		# display wanted windows
-		if windows == 1 or windows == 2:
-			
-			frame = np.rot90(np.rot90(frame)) # Rotate frame
-			cv2.imshow('tracking',frame)
-
-			if windows == 2:
-				hthresh = cv2.inRange(np.array(hue), np.array(lowerbound[0]), np.array(upperbound[0]))
-				sthresh = cv2.inRange(np.array(sat), np.array(lowerbound[1]), np.array(upperbound[1]))
-				vthresh = cv2.inRange(np.array(val), np.array(lowerbound[2]), np.array(upperbound[2]))
-				
-				hthresh = np.rot90(np.rot90(hthresh)) # Rotate frame
-				sthresh = np.rot90(np.rot90(sthresh)) # Rotate frame
-				vthresh = np.rot90(np.rot90(vthresh)) # Rotate frame
-				
-				#Show the result in frames
-				cv2.imshow('HueComp',hthresh)
-				cv2.imshow('SatComp',sthresh)
-				cv2.imshow('ValComp',vthresh)
-				cv2.imshow('closing',closing)
-
-		# exit when 'q' key is pressed
-		k = cv2.waitKey(1) & 0xFF
-		if k == ord("q"):
-			break
-
-	#GPIO.cleanup()
-	cap.release()
-	cv2.destroyAllWindows()
+#####################
+# RUN MAIN FUNCTION #
+#####################
 
 if __name__ == "__main__":
 	main()
