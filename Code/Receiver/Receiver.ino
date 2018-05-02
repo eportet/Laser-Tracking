fa33510bb0a9e1f8d9ca10cab02d54914cc2e9fa
Christopher Liao <christopher210634@gmail.com>
2018-05-02 08:14:08 -0400

    Update to Reciever side and Transmitter side code. First Working commit with particle on Transmitter.

--- Code/Receiver/Receiver.ino
+++ Code/Receiver/Receiver.ino
@@ -1,30 +1,28 @@
 SYSTEM_MODE(MANUAL);
-
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "particle-rf24.h"
 #include "math.h"
 
+
 const int enable_pin  = D7; // (pin 1) pin to enable MEMS output
 const int fclk_pin    = D0; // (pin 2) pin to set frequency of Bessel low pass filter
-
 const int ss          = A2; // SS or CSN for MEMS
 const int ss_radio    = D5; // SS or CSN for radio
 const int sck         = A3; // (pin 6)
-const int mosi        = A5; // (pin 4)
+const int mosi         = A5; // (pin 4)
                             // Gnd (pin 3 and pin 5)
                             // +5V (pin 7)
 const int ce          = D6; // to set TX or RX modes for radio
+const int azimuth_pin = D2;
+const int pitch_pin   = D3;
 
-const int mon_pin     = WKP; // WKP is monitor pin for Lidar
-
-const int azimuth_pin = TX;
-const int pitch_pin   = RX;
-
+/* Radio setup */
 RF24 radio(ce, ss_radio); // CE, CSN
-const byte address[][6] = {"00001","00002"};
+byte addresses[][6] = {"1Node","2Node"};  
 int8_t radio_data[2] = {0,0};
 
+/* MEMS Setup */
 int full_reset                  = 2621441; //Initialization to set up DAC
 int enable_internal_reference   = 3670017; //Initialization to set up DAC
 int enable_all_dac_channels     = 2097167; // B00100000 00000000 00001111//Initialization to set up DAC
@@ -40,17 +38,12 @@ int disable_all_dac_channels    = 2097215; //Power Down
 #define AD56X4_CHANNEL_C                               2 //B00000010
 #define AD56X4_CHANNEL_D                               3 //B00000011
 #define AD56X4_CHANNEL_ALL                             7 //B00000111
-
-#define pi 3.1415926
-#define BUF_SIZE 100
-
 // commands to write to LDAC buffer, then at channel_D write to DAC register
 int writecommand[]  = {AD56X4_COMMAND_WRITE_INPUT_REGISTER  | AD56X4_CHANNEL_A,
                        AD56X4_COMMAND_WRITE_INPUT_REGISTER  | AD56X4_CHANNEL_B,
                        AD56X4_COMMAND_WRITE_INPUT_REGISTER  | AD56X4_CHANNEL_C,
                        AD56X4_COMMAND_WRITE_INPUT_REGISTER_UPDATE_ALL  | AD56X4_CHANNEL_D
                      };
-
 // Set mirror dependent values
 int freq            = 650; // 650 Hz recommended cutoff frequency
 int fclk_freq       = 60*freq; //39000;    // Recommended 650 Hz * 60 - do not change!!!!!!
@@ -59,35 +52,32 @@ int bias_voltage    = 80;       // Datasheet spec - do not change!!!!!!
 double analog_bias  = (bias_voltage*65535)/160 + 0.5;
 int dig_bias        = (int) analog_bias;
 int data;
-int pitch_servo_pos   = 1800; // level
-int azimuth_servo_pos = 1500; // midway point
-int azimuth_ctl_i_acc = 0;
-int azimuth_ctl_d_last = 0;
+
+/* Change to modify velocity filter buffer size */
+#define BUF_SIZE 100
+
+/* Servo related parameters */
+double pitch_servo_pos   = 1100; // level
+double azimuth_servo_pos = 1500; // midway point
 Servo pitch_servo, azimuth_servo;
 
-// Required constants
-volatile double difX, difY, incX, incY;
-const double inc = 0.2;
-int right = 0;
-int up = 0;
-double bufX[BUF_SIZE] = {};
-double bufY[BUF_SIZE] = {};
-int p = 0;
-int q = 1;
-int serial_counter = 0;
-const double ki=0, kp=1, kd=0;
+/* Control related varaibles and parameters */
+volatile double difX, difY;  // Current position of MEMS is volts
+volatile double incX, incY;  // Amount to move MEMS by in volts.
+const double inc = 0.2;      // Tunable parameter, higher --> Respond faster to input but risk 
+                             // oscillatory behavior
+double bufX[BUF_SIZE] = {};  // circular velocity filter X
+double bufY[BUF_SIZE] = {};  // circular velocity filter Y
+int p = 0;                   // pointer to circular buffer 
+int q = 1;                   // pointer to next circular buffer value
+int serial_counter = 0;      // Delay counter to output debug info once in a while
 
-double pulseWidth=0.0; // radial distance
-double az=0.0; // azimuth angle of laser
-double el=0.0; // elevation angle of laser
-double h=0.0; // sqrt(x^2+y^2)
-double x=0.0;
-double y=0.0;
-double z=0.0;
+// Tunable parameters: Need to offset rest position, because unmovable laser beam at 0,0
+const double initX = -15;
+const double initY = 15;
 
-void azimuth_write(int pos);
-int azimuth_controller_update(double mems_pos);
-void pitch_write(int pos);
+// Tune for Servo response
+const double servo_inc = 0.01;
 
 void setup() {
   pinMode(enable_pin, OUTPUT);
@@ -113,113 +103,82 @@ void setup() {
   Serial.begin();
   
   radio.begin();
-  radio.openReadingPipe(0, address[0]);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.setAutoAck(0,false);
+  radio.openReadingPipe(0, addresses[0]);
+  radio.enableDynamicPayloads();
+  radio.setPALevel(RF24_PA_MAX);          // Transmit at maximum power
+  radio.setAutoAck(0,false);              // Disable AutoAck
   radio.startListening();
 
-  //pitch_servo.attach(pitch_pin);
   azimuth_servo.attach(azimuth_pin);
-  //pitch_servo.writeMicroseconds(pitch_servo_pos);
-  azimuth_servo.writeMicroseconds(azimuth_servo_pos);
-  delay(3000); //wait for servos to move into position
-  
+  pitch_servo.attach(pitch_pin);
+  azimuth_servo.writeMicroseconds((int)azimuth_servo_pos);
+  pitch_servo.writeMicroseconds((int)pitch_servo_pos);
 }
 
 void loop() {
   // Set analog voltage X+, X-, Y+, Y- from center (analog_bias) always at 80 V
   // Max voltage is 157 so max analog voltage is 157 - if you go beyond 157, the DAC will set to 80 V and your code won't work.
 
-  difX = 0; //Can be positive or negative
-  difY = 0;
+  /* Control system initialization. Start laser beam at offset. */
   incX = 0;
   incY = 0;
+  difX = initX;
+  difY = initY;
+  for (unsigned int k = 0 ; k < BUF_SIZE ; k++){
+    bufX[k] = initX;
+    bufY[k] = initY;
+  }
   setChannels(difX, difY);
 
+  /* Main Control Loop 
+   * IMPORTANT NOTE: 
+   * If you add code to make this loop longer, you must test to make sure 
+   * the loop in transmitter is still longer the control loop.
+   * Otherwise, the control loop will fall behind in reading transmissions,
+   * the radio buffer will go bazerk and things won't work.
+   */
   while(1) {
     while(! radio.available()); //wait for new communication
     radio.read(&radio_data, sizeof(radio_data[0])*2);
+
+    /* incX and incY are the velocity vector of the target
+     * based on an average of past 100 observed positions.
+     * Measure in volts per time interval.
+     * Time interval is amount of time it takes for one while loop
+     */
     p++;
+    if (p>=BUF_SIZE) p=0;
+    q = p+1;
+    if (q>=BUF_SIZE) q=0;
     bufX[p] = difX;
     bufY[p] = difY;
-    q = p+1;
-    if (p>=BUF_SIZE) p=0;
-    if (q>=BUF_SIZE) q=0;
-    
-    pulseWidth = pulseIn(WKP, HIGH); // Count how long the pulse is high in microseconds
-    pulseWidth = pulseWidth/1000; // Get radial distance in meters
-
     incX = (bufX[p]-bufX[q])/BUF_SIZE;
     incY = (bufY[p]-bufY[q])/BUF_SIZE;
     difX += incX;
     difY += incY;
     difX += (double)(radio_data[0])/127.0 * inc;
     difY += (double)(radio_data[1])/127.0 * inc;
-
-    az=difX*(2.0/pi*360.0/6*50); // volts to radians
-    el=difY*(2.0/pi*360.0/6*50); // volts to radians
-    h=pulseWidth*cos(el);
-    z=pulseWidth*sin(el);
-    //incX=atan(radio_data[0]/radio_data[1])/2.0/pi*360.0/6*50;
-    //incY=atan(z/sqrt(radio_data[0]^2+radio_data[1]^2))/(2.0/pi*360.0/6*50); //radians to volts
-    
-    azimuth_servo_pos = azimuth_controller_update(difX);
-    azimuth_write(azimuth_servo_pos);
-    /*
-    if (difX > 10){
-      azimuth_servo_pos ++;
-      azimuth_write(azimuth_servo_pos);
-    }
-    else if (difX < -10){
-      azimuth_servo_pos --;
-      azimuth_write(azimuth_servo_pos);
-    }
-    */
-    
     setChannels(difX, difY);
 
-    //Serial.print(radio_data[0]);
-    //Serial.print(',');
-    //Serial.println(radio_data[1]);
-    
-    serial_counter++;
+    /* Simple servo control algo */
+    azimuth_servo_pos += (difY-initY) * servo_inc;
+    if (azimuth_servo_pos > 2000 or azimuth_servo_pos < 800) continue;
+    azimuth_servo.writeMicroseconds((int)azimuth_servo_pos);
+    pitch_servo_pos -= (difX-initX) * servo_inc;
+    if (pitch_servo_pos > 2000 or pitch_servo_pos < 800) continue;
+    pitch_servo.writeMicroseconds((int)pitch_servo_pos);
+    /*serial_counter++;
     if (serial_counter > 50){
       Serial.print(difX);
       Serial.print(',');
-      Serial.print(difY);
-      Serial.print(';');
-      Serial.println(azimuth_servo_pos);
+      Serial.println(difY);
       serial_counter = 0;
-    }
+    }*/
   }
   powerdown(); //Always power down at the end to disable the MEMS output.. If you don't do this and disconnect the power you may break the MEMS
 }
 
-int azimuth_controller_update(double mems_pos) {
-    const double ki = 0.0, kp=0.1, kd=0.0;
-    static double err_i = 0;
-    static double mems_last = 0;
-    double err_p, err_d;
-
-    err_i += mems_pos;
-    err_p = mems_pos;
-    err_d = mems_pos - mems_last;
-
-    mems_last = mems_pos;
-
-    return (int)(1500 + (err_i * ki) + (err_p * kp) + (err_d * kd));
-}
-
-void azimuth_write(int pos){
-  if (pos < 1200 or pos > 1800) return;
-  azimuth_servo.writeMicroseconds(pos);
-}
-void pitch_write(int pos){
-  if (pos < 1600 or pos > 2000) return;
-  pitch_servo.writeMicroseconds(pos);
-}
-
-// All functions for this to work
+/* MEMS Functions below */
 void initialization() {         //Initialization to set up DAC
 
     digitalWrite(ss,LOW);
--- Code/Transmitter/Transmitter.ino
+++ Code/Transmitter/Transmitter.ino
@@ -11,12 +11,16 @@ SYSTEM_MODE(MANUAL);
 
 /* Phton Pin Mapping 
  *  
+ *  IMPORTANT:
+ *  *** Connect A0 to D0 to trigger reset of mux index at falling edge of MSB
+ *  *** Connect WKP to D4 to trigger read of max out pin at value change of LSB
+ *  
  * Multiplexer pins:
- * BIT_16_PIN --> D0
- * BIT_8_PIN  --> D1
- * BIT_4_PIN  --> D2
- * BIT_2_PIN  --> D3
- * BIT_1_PIN  --> D4
+ * BIT_16_PIN --> D0 --> A0 --> TIM4_CH2          (MSB)
+ * BIT_8_PIN  --> D1 --> TIM4_CH1
+ * BIT_4_PIN  --> D2 --> TIM3_CH2
+ * BIT_2_PIN  --> D3 --> TIM3_CH1
+ * BIT_1_PIN  --> D4 --> WKP --> TIM5_CH1  (LSB)
  * OUT_PIN    --> D5
  * 
  * pins for radio:
@@ -39,6 +43,7 @@ SYSTEM_MODE(MANUAL);
 #define X_MAX      9.25
 #define Y_MAX      6.25
 #define SIGNAL_PIN D7
+#define RESET_PIN  A0
 
 const int ss          = A2; // SS or CSN for MEMS
 const int sck         = A3; // (pin 6)
@@ -48,36 +53,47 @@ const int ce          = D6; // to set TX or RX modes for radio
 /* This is the diode number .
  * The mux index is offset by 5. For example diode #3 --> 8 on mux
  *  | | <-- 5
- * 4  3   6  5      ^ This way up (such that array is at bottom right of board)
- *   8  7  10  9
- * 12 11  14 13
- *   16 15  18 17 
- * 20 19  22 21
- *   24 23  26 25
- */
-
-/* dx and dy indexed by diode #, first three positions are not used
+ * 29  30  27  28      ^ This way up (such that array is at bottom right of board)
+ *   5   6   3   4
+ * 13 14  11  12
+ *   9  10   7   8 
+ * 1   2  31  0
+ *   25  26  23 24 
+ *   
+ * -9.25   -4.25     1.75   6.75 
+ *   -6.75    -1.75   4.25     9.25
+ *   
+ * dx and dy indexed by diode #.
  * Diode # is shown above. 
  * dx and dy is distance from x and y axes in millimeters
  * origin is in center of photodiode array.
  */
-const double dx[] = {-6.75, -1.75, 4.25, 9.25,
-                     -9.25, -4.25, 1.75, 6.75,
-                     -9.25, -4.25, 1.75, 6.75,
-                     -6.75, -1.75, 4.25, 9.25,
-                     -6.75, -1.75, 4.25, 9.25,
-                     -9.25, -4.25, 1.75, 6.75 };
-const double dy[] = {-1.25, -1.25, -1.25, -1.25,
-                     1.25, 1.25, 1.25, 1.25,
-                     -3.75, -3.75, -3.75, -3.75,
-                     3.75, 3.75, 3.75, 3.75,
-                     -6.25, -6.25, -6.25, -6.25,
-                     6.25, 6.25, 6.25, 6.25};             
+ 
+const double dx[] = {6.75, -9.25, -4.25, 4.25,   // 0 1 2 3 
+                     9.25, -6.75, -1.75, 4.25,   // 4 5 6 7
+                     9.25, -6.75, -1.75, 1.75,   // 8 9 10 11
+                     6.75, -9.25, -4.25, 0,      // 12 13 14 15
+                     0, 0, 0, 0,                 // 16 17 18 19
+                     0, 0, 0, 4.25,              // 20 21 22 23
+                     9.25, -6.75, -1.75, 1.75,   // 24 25 26 27
+                     6.75, -9.25, -4.25, 1.75};  // 28 29 30 31
+
+const double dy[] = {-3.75, -3.75, -3.75,         // 0 1 2
+                     3.75, 3.75, 3.75, 3.75,      // 3 4 5 6
+                     -1.25, -1.25, -1.25, -1.25,  // 7 8 9 10
+                     1.25, 1.25, 1.25, 1.25,      // 11 12 13 14
+                     0, 0, 0, 0, 0, 0, 0, 0,      // 15 16 17 18 19 20 21 22
+                     -6.25, -6.25, -6.25, -6.25,  // 23 24 25 26
+                     6.25, 6.25, 6.25, 6.25,      // 27 28 19 30
+                     -3.75};                      // 31
+             
 RF24 radio(ce, ss); // CE, CSN
 byte addresses[][6] = {"1Node","2Node"};  
-int    threshold, number_diodes_on;
+int number_diodes_on;
 int8_t data[2] = {0,0};
 double dy_mean, dx_mean, dy_sum, dx_sum;
+volatile bool mux_array[32];
+volatile unsigned char mux_index = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -88,28 +104,23 @@ void setup() {
   radio.setAutoAck(0,false);
   radio.stopListening();
 
-  pinMode(BIT_16_PIN, OUTPUT);
-  pinMode(BIT_8_PIN,  OUTPUT);
-  pinMode(BIT_4_PIN,  OUTPUT);
-  pinMode(BIT_2_PIN,  OUTPUT);
-  pinMode(BIT_1_PIN,  OUTPUT);
+  TC_setup();
+  pinMode(BIT_1_PIN,  INPUT_PULLUP);
   pinMode(OUT_PIN,    INPUT_PULLUP);
+  pinMode(RESET_PIN ,INPUT_PULLUP);
   pinMode(SIGNAL_PIN, INPUT_PULLUP);
+  attachInterrupt(RESET_PIN, mux_index_reset, FALLING);
+  attachInterrupt(BIT_1_PIN, mux_read, CHANGE);
+  for (int i = 0 ; i < 32 ; i++) mux_array[i] = 0;
 }
 
 void loop() {
   dx_sum = 0;
   dy_sum = 0;
   number_diodes_on = 0;
-
-  // iterate through mux
-  for (int i=8 ; i < 32 ; i++){
-    digitalWrite(BIT_1_PIN,  i & 1     );
-    digitalWrite(BIT_2_PIN,  i & (1<<1));
-    digitalWrite(BIT_4_PIN,  i & (1<<2));
-    digitalWrite(BIT_8_PIN,  i & (1<<3));
-    digitalWrite(BIT_16_PIN, i & (1<<4));
-    if (digitalRead(OUT_PIN) == HIGH){
+  //unsigned long a = micros();
+  for (int i = 0 ; i < 32 ; i++){
+    if (mux_array[i]){
       dx_sum += dx[i];
       dy_sum += dy[i];
       number_diodes_on ++;
@@ -125,10 +136,95 @@ void loop() {
   }
   data[0] = round(dx_mean/X_MAX * 127);
   data[1] = round(dy_mean/Y_MAX * 127);
-  //Serial.print(data[0]);
-  //Serial.print(',');
-  //Serial.println(data[1]);
-  //delay(100);
+  //unsigned long b = micros();
+  
+  //Serial.println(b-a);
+
+  // Serial Statements somewhat intentional to induce necessary delay
+  Serial.print(data[0]);
+  Serial.print(',');
+  Serial.println(data[1]);
   radio.write(&data, sizeof(data[0])*2);
 }
+
+void mux_read(){
+  mux_array[mux_index] = digitalRead(OUT_PIN);
+  mux_index++;
+  if (mux_index > 31) mux_index = 0;
+}
+
+void mux_index_reset(){
+  mux_index = 0;
+}
+
+/* Setup timer counters in a configurations such that 
+ * LSB waveform has highest frequency and MSB waveform has lowest freuency,
+ * Each bit's frequency is exactly half the lower bit's frequency.
+ * 
+ * Note: The bit order is not linear from 0 to 31, it jumps around
+ */
+void TC_setup()
+{
+  // Enable TIM 3,4,5 peripheral
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+
+  // 120Mhz master clock
+
+  //Synchronize
+  TIM3->SMCR = RESET;
+  TIM4->SMCR = RESET;
+  TIM5->SMCR = RESET;
+
+  // TIM8 is master of TIM5 is master of TIM3 is master of TIM4
+  TIM5->CR2   |= TIM_CR2_MMS_1;  // Mster mode
+  TIM3->CR2   |= TIM_CR2_MMS_2;  // Mster mode
+  TIM3->SMCR  |= TIM_SMCR_SMS_0 | TIM_SMCR_SMS_1 | TIM_SMCR_SMS_2; // Slave mode
+  TIM4->SMCR  |= TIM_SMCR_SMS_0 | TIM_SMCR_SMS_1 | TIM_SMCR_SMS_2; // Slave mode
+  TIM3->SMCR  |= TIM_SMCR_TS_1; // Internal clock signal routing Table 60.
+  TIM4->SMCR  |= TIM_SMCR_TS_1; // Internal clock signal routing Table 60.
+  
+  // Configure TIM3
+  TIM3->ARR    = 3;
+  TIM3->CCR1   = 2;  // Compare capture reg 1 bit 4
+  TIM3->CCR2   = 2;  // Compare capture reg 2 bit 2
+  TIM3->CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; // PWM mode Channel 1
+  TIM3->CCMR1 |= TIM_CCMR1_OC2M_0 | TIM_CCMR1_OC2M_1; // Toggle mode Channel 2
+  TIM3->CCER  |= TIM_CCER_CC1E | TIM_CCER_CC2E; //Capture Compare Enable channel 1 and 2
+  TIM3->PSC    = 0;  // prescale clock
+  TIM3->CR1   |= TIM_CR1_CEN; //Counter enable
+
+  // Configure TIM4
+  TIM4->ARR    = 3;
+  TIM4->CCR1   = 2;  // Compare capture reg 1 bit 16 (MSB)
+  TIM4->CCR2   = 2;  // Compare capture reg 2 bit 8
+  TIM4->CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 ; // PWM mode Channel 1
+  TIM4->CCMR1 |= TIM_CCMR1_OC2M_0 | TIM_CCMR1_OC2M_1 ; // Toggle mode Channel 2
+  TIM4->CCER  |= TIM_CCER_CC1E | TIM_CCER_CC2E; //Capture Compare Enable channel 1 and 2
+  TIM4->PSC    = 0;  // prescale clock
+  TIM4->CR1   |= TIM_CR1_CEN; //Counter enable
+
+  // Configure TIM5
+  TIM5->ARR    = 64;
+  TIM5->CCR1   = 0;  // Compare capture reg 1 bit 1 (LSB)
+  TIM5->CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1; // Toggle mode Channel 1
+  TIM5->CCER  |= TIM_CCER_CC1E ; //Capture Compare Enable channel 1
+  TIM5->PSC    = 16;  // prescale clock
+  TIM5->CR1   |= TIM_CR1_CEN; //Counter enable
+
+  // Additional setup
+  HAL_Pin_Mode(D0,AF_OUTPUT_PUSHPULL);
+  HAL_Pin_Mode(D1,AF_OUTPUT_PUSHPULL);
+  HAL_Pin_Mode(D2,AF_OUTPUT_PUSHPULL);
+  HAL_Pin_Mode(D3,AF_OUTPUT_PUSHPULL);
+  HAL_Pin_Mode(WKP,AF_OUTPUT_PUSHPULL);
+  pinMode(D4, INPUT);
+  GPIO_PinAFConfig(PIN_MAP[D3].gpio_peripheral,PIN_MAP[D3].gpio_pin_source,GPIO_AF_TIM3);
+  GPIO_PinAFConfig(PIN_MAP[D2].gpio_peripheral,PIN_MAP[D2].gpio_pin_source,GPIO_AF_TIM3);
+  GPIO_PinAFConfig(PIN_MAP[D0].gpio_peripheral,PIN_MAP[D0].gpio_pin_source,GPIO_AF_TIM4);
+  GPIO_PinAFConfig(PIN_MAP[D1].gpio_peripheral,PIN_MAP[D1].gpio_pin_source,GPIO_AF_TIM4);
+  GPIO_PinAFConfig(PIN_MAP[WKP].gpio_peripheral,PIN_MAP[WKP].gpio_pin_source,GPIO_AF_TIM5);
+}
+
 
